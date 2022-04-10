8f66a8cf67c1969174fb98f0b11eebb3f89143e5
adcorrea <antoniopucc@hotmail.com>
2022-04-10 13:12:05 -0300

    Post incluido

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const consign = require('consign');
-
+const bodyParser = require('body-parser');
 
 
 let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 consign().include('routes').into(app);
 
